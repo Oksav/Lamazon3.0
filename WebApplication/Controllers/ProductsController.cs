844c715d3cf5db6b75325231d07a6cc1e6751406
oktag <oktag@DESKTOP-72AO56J>
2020-10-07 14:50:41 +0200

    First commit

--- WebApplication/Controllers/ProductsController.cs
+++ WebApplication/Controllers/ProductsController.cs
@@ -27,10 +27,48 @@ namespace WebApplication.Controllers
         }
 
 
-        [Authorize(Roles = "admin")]
+        [Authorize]
         public IActionResult ListProducts()
         {
             return View("Index",_productService.GetAllProducts());
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public IActionResult AddProduct(ProductViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _productService.CreateProduct(model);
+                return RedirectToAction("ListProducts", "Products");
+            }
+            else
+                return View(model);
+        }
+
+        [HttpGet]
+        [Authorize(Roles ="admin")]
+        public IActionResult DeleteProduct()
+        {
+            IEnumerable<ProductViewModel> products = _productService.GetAllProducts();
+            ViewBag.Products = products;
+            return View(products);
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]        
+        public IActionResult DeleteProduct(int id)
+        {
+            _productService.RemoveProduct(id);
+            return RedirectToAction("Delete Product", "Products");
         }
 
 
